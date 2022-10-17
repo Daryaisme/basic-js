2c0a5969a7531c543e3d3be99b063e7f26b30532
Darya Suravtsova <darblnkapm@gmail.com>
2022-10-17 12:07:04 +0300

    feat: solve carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function dateSample(sampleActivity) {
+  if (isNaN(parseInt(sampleActivity)) || parseInt(sampleActivity) <= 0 || parseFloat(sampleActivity) > MODERN_ACTIVITY || typeof(sampleActivity) !== 'string' || typeof(sampleActivity) === 'Infinity')
+    return false
+  else {
+      return t = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / Math.log(2) * HALF_LIFE_PERIOD)
+  }
+ }
+
 
 module.exports = {
   dateSample
